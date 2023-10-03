4b64853377e18dd935be9ad72829fccf395b89f0
Jroldan- <javierroldanpoces@gmail.com>
2023-10-03 13:05:54 +0200

    solucionado data races printf routine_checker y join solo un thread

--- src/actions.c
+++ src/actions.c
@@ -6,7 +6,7 @@
 /*   By: javier <javier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 11:53:07 by javi              #+#    #+#             */
-/*   Updated: 2023/10/02 21:34:17 by javier           ###   ########.fr       */
+/*   Updated: 2023/10/03 13:01:39 by javier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 void	ft_take_forks(t_philosopher *philo)
 {
+	if (philo->project->nbr_philo == 1)
+	{
+		printf("nbr_philo 1\n");
+		return ;
+	}
 	pthread_mutex_lock(&philo->fork_left);
 	printf("%ld %d has taken a fork\n", (long)timestamp(philo->project), philo->id);
 	pthread_mutex_lock(philo->fork);
@@ -22,11 +27,16 @@ void	ft_take_forks(t_philosopher *philo)
 
 void	ft_eat(t_philosopher *philo)
 {
+	if (philo->project->nbr_philo == 1)
+	{
+		printf("nbr_philo 1\n");
+		return ;
+	}
 	printf("%ld %d is eat\n", (long)timestamp(philo->project), philo->id);
 	pthread_mutex_lock(&philo->mute_lock);
 	philo->count_foods++;
-	printf("philo %d count_foods %d\n", philo->id, philo->count_foods);
-	philo->last_food = get_time();
+	//printf("philo %d count_foods %d\n", philo->id, philo->count_foods);
+	philo->last_food = get_time(); // aqui da el data race cuando no dead
 	sleep_time(philo->project->time_to_eat, philo->project);
 	pthread_mutex_unlock(&philo->mute_lock);
 	pthread_mutex_unlock(&philo->fork_left);
@@ -35,6 +45,11 @@ void	ft_eat(t_philosopher *philo)
 
 void	ft_sleep(t_philosopher *philo)
 {
+	if (philo->project->nbr_philo == 1)
+	{
+		printf("nbr_philo 1\n");
+		return ;
+	}
 	printf("%ld %d sleeping\n", (long)timestamp(philo->project), philo->id);
 	sleep_time(philo->project->time_to_sleep, philo->project);
 }
